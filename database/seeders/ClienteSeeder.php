dec348250b090e1442bf44591b59ea2a0f26e647
wylliamsena <wylliamsena80@gmail.com>
2025-04-10 12:34:36 -0300

    inplementado, model, migrate, Index, seed do crud de Clientes

---
+++ database/seeders/ClienteSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cliente;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClienteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Cliente::create([
+            'nome' => 'Cliente Exemplo1',
+            'endereco' => 'Rua Exemplo1, 111',
+            'telefone' => 'Rua Exemplo1, 111',
+            'cpf' => 'Rua Exemplo1, 111',
+            'email' => 'Rua Exemplo1, 111',
+            'senha' => 'Rua Exemplo1, 111',
+        ]);
+
+        Cliente::create([
+            'nome' => 'Cliente Exemplo2',
+            'endereco' => 'Rua Exemplo2, 222',
+            'telefone' => 'Rua Exemplo2, 222',
+            'cpf' => 'Rua Exemplo2, 222',
+            'email' => 'Rua Exemplo2, 222',
+            'senha' => 'Rua Exemplo2, 222',
+        ]);
+    }
+}
